aac5264b9cf1c392a7e425d9e5924b942506556b
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-10-23 15:44:08 +0330

    fix : time_conver function added

--- build/lib/pyVerilog/__main__.py
+++ build/lib/pyVerilog/__main__.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 from .pyVerilog import *
 import sys
+import time
 if __name__=="__main__":
     args=sys.argv
     filename=""
     if len(args)>2:
+        timer_1=time.perf_counter()
         if args[2].upper()=="ALL":
             getVerilog(args[1],alltest=True)
         else:
             getVerilog(args[1],input_data=args[2].split(","),alltest=False)
+        timer_2=time.perf_counter()
+        print("Simulation Time : "+time_convert(str(timer_2-timer_1)))
     else:
         print("Bad pyVerilog Call")
