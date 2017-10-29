15b4f7e1796e3d3cfaf0e8090196c9c8ef8637d4
sepandhaghighi <sepand.haghighi@yahoo.com>
2017-10-29 19:25:59 +0330

    fix : setup file version modified

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'verilogparser',
   packages = ['verilogparser'],
-  version = '0.1',
+  version = '0.11',
   description = 'Python Verilog File Parser',
   long_description='''Python Verilog File Parser''',
   author = 'Sepand Haghighi',
   author_email = 'sepand@qpage.ir',
   url = 'https://github.com/sepandhaghighi/verilogparser',
-  download_url = 'https://github.com/sepandhaghighi/verilogparser/tarball/v0.1',
+  download_url = 'https://github.com/sepandhaghighi/verilogparser/tarball/v0.11',
   keywords = ["Verilog","python","HDL","ISCAS","script","parser","simulation"],
   install_requires=[
       'art',
